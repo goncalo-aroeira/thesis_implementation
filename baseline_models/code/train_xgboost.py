4c8441f36c9b7ed94f8728bb8837e97cf476171f
goncalo-aroeira <goncalo.b.aroeira@gmail.com>
2025-03-11 16:14:08 +0000

    fixed xg_boost implementation

--- baseline_models/code/train_xgboost.py
+++ baseline_models/code/train_xgboost.py
@@ -1,6 +1,6 @@
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 from data_preprocessing import prepare_data
 from utils import save_model, load_model
 
@@ -19,6 +19,6 @@ save_model(xgb_model, "/home/goncalo/scgpt/baseline_models/models/xgboost_traine
 
 # Evaluate model
 y_pred = xgb_model.predict(X_test)
-rmse = mean_squared_error(y_test, y_pred, squared=False)
+rmse = root_mean_squared_error(y_test, y_pred)
 
 print(f"✅ XGBoost RMSE: {rmse:.4f}")
